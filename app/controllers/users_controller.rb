cce10ba4ca24ce1e7509cc44496e4f5f0288d1e9
Kai Lung <klung16@gmail.com>
2017-06-19 06:22:06 -0700

    Implemented new and create methods of user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,12 @@
 class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      redirect_to new_user_path
+    end
+  end
 end
