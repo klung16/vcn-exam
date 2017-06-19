eda90f3dd5229ff6d7e6023b50926929bacafba0
Kai Lung <klung16@gmail.com>
2017-06-19 07:13:54 -0700

    Add new route for information

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(params[:user])
     if @user.save
-      redirect_to new_user_path
+      redirect_to "users/info"
     end
   end
 end
