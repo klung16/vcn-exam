0423cf8ef0df3ea16ed9965e3f0682acdf57b4a6
Kai Lung <klung16@gmail.com>
2017-06-19 14:54:21 -0700

    Fix issue with redirection error after form submit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,9 +4,14 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(params[:user])
+    @user = User.new(user_params)
     if @user.save
-      redirect_to "users/info"
+      redirect_to "/users"
     end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :address, :message)
   end
 end
