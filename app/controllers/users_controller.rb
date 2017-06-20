b16d503bd3da7e5df7fa5bd639f101647d88cd26
Kai Lung <klung16@gmail.com>
2017-06-19 17:17:22 -0700

    Added Stripe code to Users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,9 +9,38 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
+    
+    #####
+    # Stripe code from https://stripe.com/docs/checkout/rails
+    #####
+
+    # Amount in cents
+    @amount = 500
+
+    customer = Stripe::Customer.create(
+      :email => params[:stripeEmail],
+      :source  => params[:stripeToken]
+    )
+
+    charge = Stripe::Charge.create(
+      :customer    => customer.id,
+      :amount      => @amount,
+      :description => 'Rails Stripe customer',
+      :currency    => 'usd'
+    )
+    
+    #####
+    # End of stripe code
+    #####
+
     if @user.save
       redirect_to "/users"
     end
+  
+
+  rescue Stripe::CardError => e
+    flash[:error] = e.message
+    redirect_to new_charge_path
   end
 
   private
