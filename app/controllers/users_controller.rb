3ecf10b660182439c9d15328f120482b1498716d
Kai Lung <klung16@gmail.com>
2017-06-19 21:26:32 -0700

    Fixed so that only the last entry shows up on the map

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   def index
     @users = User.all
-    @hash = Gmaps4rails.build_markers(@users) do |user, marker|
+    @hash = Gmaps4rails.build_markers(@users.order("created_at").last) do |user, marker|
       marker.lat user.latitude
       marker.lng user.longitude
     end
