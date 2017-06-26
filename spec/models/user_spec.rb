16306f9ded1ce7d4ae80f8d3dc01ce5f9b0d060b
klung16 <klung16@gmail.com>
2017-06-26 15:51:40 -0700

    Fixed email validation test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,14 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  @user = FactoryGirl.create :user
+  user = FactoryGirl.create :user
 
   describe 'validation' do
     it 'validates the email' do
-      expect(ValidatesEmailFormatOf::validate_email_format("klung16@gmail.com")).to be_nil
-      #@user.email = "hello"
+      expect(ValidatesEmailFormatOf::validate_email_format(user.email)).to be_nil
+      user.email = "hello"
       email_err = ["does not appear to be a valid e-mail address"]
-      expect(ValidatesEmailFormatOf::validate_email_format("klung16")).to eq email_err
+      expect(ValidatesEmailFormatOf::validate_email_format(user.email)).to eq email_err
     end
   end
 end
