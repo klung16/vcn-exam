184a7c864122c4603cfc0509b59dbe499a134871
klung16 <klung16@gmail.com>
2017-06-26 16:01:34 -0700

    Improved email validation test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,10 +5,9 @@ RSpec.describe User, type: :model do
 
   describe 'validation' do
     it 'validates the email' do
-      expect(ValidatesEmailFormatOf::validate_email_format(user.email)).to be_nil
-      user.email = "hello"
-      email_err = ["does not appear to be a valid e-mail address"]
-      expect(ValidatesEmailFormatOf::validate_email_format(user.email)).to eq email_err
-    end
+      expect(user).to be_valid
+      user.email = "hello@"
+      expect(user).not_to be_valid
+      end
   end
 end
