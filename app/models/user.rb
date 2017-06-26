599589b3fff13e154016c66bfd17034abd8199aa
klung16 <klung16@gmail.com>
2017-06-26 16:25:53 -0700

    Improved email validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   geocoded_by :address
   after_validation :geocode
   validates :name, presence: true
-  validates :email, presence: true, email_format: {message: "Not a valid email address!"}
+  validates :email, email_format: {message: "Not a valid email address!"}
   validates :address, presence: true
   validates :message, presence: true
 end
