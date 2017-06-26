f8af8dd7ea596ede798d692a2eabadde6c9aaab4
klung16 <klung16@gmail.com>
2017-06-26 15:48:13 -0700

    Hardcoded user email address to test rspec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,9 +5,10 @@ RSpec.describe User, type: :model do
 
   describe 'validation' do
     it 'validates the email' do
-      expect(ValidatesEmailFormatOf::validate_email_format(@user.email)).to be_nil
-      @user.email = "hello"
-      expect(ValidatesEmailFormatOf::validate_email_format(@user.email)).to eq "Not a valid email address!"
+      expect(ValidatesEmailFormatOf::validate_email_format("klung16@gmail.com")).to be_nil
+      #@user.email = "hello"
+      email_err = ["does not appear to be a valid e-mail address"]
+      expect(ValidatesEmailFormatOf::validate_email_format("klung16")).to eq email_err
     end
   end
 end
