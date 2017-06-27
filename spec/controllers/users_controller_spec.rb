8a42aaca2f72166a5c74d0a838f5bd7f1dc8e20d
klung16 <klung16@gmail.com>
2017-06-27 15:49:47 -0700

    Added Users controller tests

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,7 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
+  before (:each) do
+    @user = FactoryGirl.create :user
+  end
+
   describe 'GET index' do
+    it 'creates an array of users' do
+      get :index
+      expect(assigns(:users)).to eq([@user])
+    end
+
     it 'renders the index template' do
       get :index
       expect(response).to render_template('index')
