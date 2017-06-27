f4d565ad1c6cee900e9bcd09d0de7ca6556d6119
klung16 <klung16@gmail.com>
2017-06-26 17:17:39 -0700

    Added user model validation tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,13 +1,64 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  user = FactoryGirl.create :user
+  before (:each) do
+    @user = FactoryGirl.create :user
+  end
 
   describe 'validation' do
-    it 'validates the email' do
-      expect(user).to be_valid
-      user.email = "hello@"
-      expect(user).not_to be_valid
+
+    context 'when the name is valid' do
+      it 'accepts the name' do
+        expect(@user).to be_valid
       end
+    end
+
+    context 'when the name is invalid' do
+      it 'rejects the name' do
+        @user.name = ""
+        expect(@user).not_to be_valid
+      end
+    end
+
+    context 'when the email is valid' do
+      it 'accepts the email' do
+        expect(@user).to be_valid
+      end
+    end
+
+    context 'when the email is invalid' do
+      it 'rejects the email' do
+        @user.email = "hello@"
+        expect(@user).not_to be_valid
+        @user.name = ""
+        expect(@user).not_to be_valid
+      end
+    end
+
+    context 'when the address is valid' do
+      it 'accepts the address' do
+        expect(@user).to be_valid
+      end
+    end
+
+    context 'when the address is invalid' do
+      it 'rejects the address' do
+        @user.address = ""
+        expect(@user).not_to be_valid
+      end
+    end
+
+    context 'when the message is valid' do
+      it 'accepts the message' do
+        expect(@user).to be_valid
+      end
+    end
+
+    context 'when the message is invalid' do
+      it 'rejects the message' do
+        @user.message = ""
+        expect(@user).not_to be_valid
+      end
+    end
   end
 end
