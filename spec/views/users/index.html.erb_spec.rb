ad390cb4483242972a854f94c75b2c5e91393ff1
klung16 <klung16@gmail.com>
2017-06-28 16:08:31 -0700

    Created tests for users/index

--- spec/views/users/index.html.erb_spec.rb
+++ spec/views/users/index.html.erb_spec.rb
@@ -1,5 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe "users/index", type: :view do
+  before (:each) do
+    @user = FactoryGirl.create :user
+    @users = User.all
+  end
+
+  context 'when the user is valid' do
+    it 'displays user information correctly' do
+      render
+      expect(rendered).to include("Enter new user information!")
+      expect(rendered).to include("Display the last user's information:")
+      expect(rendered).to include("Name: #{@user.name.titleize}")
+      expect(rendered).to include("Email Address: #{@user.email}")
+      expect(rendered).to include("Mailing Address: #{@user.address.titleize}")
+      expect(rendered).to include("Message: #{@user.message}")
+    end
+  end
+
+  context 'when the user is invalid' do
+    it 'does not display the user information' do
+      @users = []
+      render
+      expect(rendered).to include("Enter new user information!")
+      expect(rendered).not_to include("Display the last user's information:")
+      expect(rendered).not_to include("Name: #{@user.name.titleize}")
+      expect(rendered).not_to include("Email Address: #{@user.email}")
+      expect(rendered).not_to include("Mailing Address: #{@user.address.titleize}")
+      expect(rendered).not_to include("Message: #{@user.message}")
+    end
+  end
 
 end
