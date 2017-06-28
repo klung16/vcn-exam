82a63673a4cec5207efacd7a71092adf36888648
klung16 <klung16@gmail.com>
2017-06-28 16:20:36 -0700

    Cleaned up users/index test file

--- spec/views/users/index.html.erb_spec.rb
+++ spec/views/users/index.html.erb_spec.rb
@@ -1,16 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe "users/index", type: :view do
-  before (:each) do
-    @user = FactoryGirl.create :user
-    @users = User.all
-  end
+
 
   context 'when the user is valid' do
-    it 'displays user information correctly' do
+    before (:each) do
+      @user = FactoryGirl.create :user
+      @users = User.all
       render
+    end
+
+    it 'displays the proper headers' do
       expect(rendered).to include("Enter new user information!")
       expect(rendered).to include("Display the last user's information:")
+    end
+    it 'displays user information correctly' do
       expect(rendered).to include("Name: #{@user.name.titleize}")
       expect(rendered).to include("Email Address: #{@user.email}")
       expect(rendered).to include("Mailing Address: #{@user.address.titleize}")
@@ -19,16 +23,22 @@ RSpec.describe "users/index", type: :view do
   end
 
   context 'when the user is invalid' do
-    it 'does not display the user information' do
+    before (:each) do
+      @user = FactoryGirl.create :user
       @users = []
       render
+    end
+
+    it 'displays the proper headers' do
       expect(rendered).to include("Enter new user information!")
       expect(rendered).not_to include("Display the last user's information:")
+    end
+
+    it 'does not display the user information' do
       expect(rendered).not_to include("Name: #{@user.name.titleize}")
       expect(rendered).not_to include("Email Address: #{@user.email}")
       expect(rendered).not_to include("Mailing Address: #{@user.address.titleize}")
       expect(rendered).not_to include("Message: #{@user.message}")
     end
   end
-
 end
