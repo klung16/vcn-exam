95b1a168a16ff6210dc2b6feffbb62741dc75979
klung16 <klung16@gmail.com>
2017-06-28 17:18:37 -0700

    Created rspec tests for users/new view

--- spec/views/users/new.html.erb_spec.rb
+++ spec/views/users/new.html.erb_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe "users/new", type: :view do
+  before (:each) do
+    @user = User.new
+  end
 
+  context 'when page is loaded' do
+    it 'displays the user information form' do
+      render
+      expect(rendered).to include("Enter the user information")
+      expect(rendered).to include("Full Name:")
+      expect(rendered).to include("Email Address:")
+      expect(rendered).to include("Mailing Address:")
+      expect(rendered).to include("Message")
+      expect(rendered).to include("Would you like to donate $5 to the Vancouver Community Network?")
+    end
+  end
 end
